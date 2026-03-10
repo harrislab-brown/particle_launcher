e0ea62a4ed6b084f7b66a357155355e655dfa062
eli-silver <eli_silver@brown.edu>
2026-03-09 20:01:05 -0400

    basic code for milled PCB is working. Switched to arduino uno

---
+++ mega_servo_release/due_shield_code/src/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+
+#define servoPin 7
+#define relayPin 5
+#define photoGate1Pin 8
+#define photoGate2Pin 10
+#define buttonPin A0
+
+int cameraTriggerDelay = 300; // Delay in milliseconds
+
+int servoRestPosition = 0; // Servo position when not triggered
+int servoReleasePosition = 90; // Servo position to release the particle
+
+Servo releaseServo;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Particle Launcher Ready!");
+
+  releaseServo.attach(servoPin);
+  releaseServo.write(servoRestPosition);
+
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH); // HIGH = relay off (active-low module)
+
+  pinMode(photoGate1Pin, INPUT);
+  pinMode(photoGate2Pin, INPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == LOW) {
+    Serial.println("Trigger detected!");
+
+    digitalWrite(relayPin, LOW);  // LOW = relay on (active-low module)
+    Serial.println(digitalRead(relayPin));
+    delay(cameraTriggerDelay);
+
+    releaseServo.write(servoReleasePosition);
+    delay(1000);
+    releaseServo.write(servoRestPosition);
+    digitalWrite(relayPin, HIGH); // HIGH = relay off
+    Serial.println(digitalRead(relayPin));
+  }
+}
